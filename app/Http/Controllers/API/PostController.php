1c6755af6e5617709dc1899a606f34d62f78d02c
cjbm2994 <cjbm2994@gmail.com>
2018-03-13 12:16:27 +0800

    Create post resource routes

---
+++ app/Http/Controllers/API/PostController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cjbm2994
+ * Date: 13/03/2018
+ * Time: 11:58 AM
+ */
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        
+    }
+}
